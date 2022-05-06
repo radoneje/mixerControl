12300992497949c62fbdcaab93ea1890b4390dec
radoneje <den.shevchenko@gmail.com>
2022-05-06 14:02:52 +0300

    ww

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -171,7 +171,7 @@ router.post("/addImageToPresFile/:id/", async (req, res) => {
     var fileRecord = await addImageToPresFolder(req.params["id"], filePath, req);
     fileRecord=await addImageLrvToPresFile(fileRecord[0].id,fileRecord[0].fullpath,req);
     console.log(fileRecord);
-    await noifyNewPresFile(filerecord[0], req)
+    await noifyNewPresFile(fileRecord[0], req)
 })
 async function noifyNewPresFile(filerecord, req){
     var eventid=(await req.knex.select("*").from("t_presfolders").where({id:fileRecord.folderid}))[0].eventid
