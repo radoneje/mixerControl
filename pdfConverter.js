155bbf4add14e5996a3cbf086f74b3406b59574b
radoneje <den.shevchenko@gmail.com>
2022-05-06 11:26:30 +0300

    ww

--- pdfConverter.js
+++ pdfConverter.js
@@ -33,7 +33,7 @@ app.use('/',async (req,res)=>{
        // .command("convert")
 
        // .out("background:transparent")
-        .selectFrame(0)
+        .selectFrame(1)
         .quality(75)
         .density(300, 300)
         .resize(1920,1080)
