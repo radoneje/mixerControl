ea5093223c90c7d8b17b8ac7f4c5af5a89f3de4c
radoneje <den.shevchenko@gmail.com>
2022-05-06 12:53:22 +0300

    ww

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -170,9 +170,10 @@ router.post("/addImageLrvToPresFile/:id/", async (req, res) => {
     await filehandle.close();
     var fileRecord = await addImageLrvToPresFile(req.params["id"], filePath, req);
 
+    var eventid=(await req.knex,select("*").from("t_presfolders").where({id:fileRecord.folderid}))[0].eventid)
     req.io.emit("message", JSON.stringify({
         cmd: "addPresImg",
-        eventid: (await req.knex,select("*").from("t_presfolders").where({id:fileRecord.folderid}))[0].eventid,
+        eventid:eventid ,
         folderid: fileRecord.folderid,
         value: {id: fileRecord[0].id, size: fileRecord[0].lrvsize}
     }))
