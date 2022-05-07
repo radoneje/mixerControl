83dfb6e507377066fe38d4d94c22d182f3e17195
radoneje <den.shevchenko@gmail.com>
2022-05-08 00:09:24 +0300

    ww

--- public/javascripts/speakerScript.js
+++ public/javascripts/speakerScript.js
@@ -28,6 +28,8 @@ var serverUrl = "wss://wowza02.onevent.online:8443";
 var SESSION_STATUS = Flashphoner.constants.SESSION_STATUS;
 var STREAM_STATUS = Flashphoner.constants.STREAM_STATUS;
 var STREAM_STATUS_INFO = Flashphoner.constants.STREAM_STATUS_INFO;
+localVideo = document.getElementById("myVideo");
+remoteVideo = document.getElementById("mixerVideo");
 
 function activeteWebCam(){
     Flashphoner.init();
@@ -43,5 +45,39 @@ function activeteWebCam(){
     });
 }
 function startStreaming(session){
+    var streamName = eventid+"_"+faceid;
+    session.createStream({
+        name: streamName,
+        display: localVideo,
+        cacheLocalResources: true,
+        receiveVideo: false,
+        receiveAudio: false
+    }).play();
+       /* .on(STREAM_STATUS.PUBLISHING, function(publishStream){
+            console.log("STREAM_STATUS.PUBLISHING");
+            //play preview
+            session.createStream({
+                name: streamName,
+                display: remoteVideo
+            }).on(STREAM_STATUS.PLAYING, function(previewStream){
+                //enable stop button
+                onStarted(publishStream, previewStream);
+            }).on(STREAM_STATUS.STOPPED, function(){
+                publishStream.stop();
+            }).on(STREAM_STATUS.FAILED, function(stream){
+                //preview failed, stop publishStream
+                if (publishStream.status() == STREAM_STATUS.PUBLISHING) {
+                    console.log("STREAM_STATUS.FAILED", stream);
+                    publishStream.stop();
+                }
+            }).play();
+        }).on(STREAM_STATUS.UNPUBLISHED, function(){
+        console.log("STREAM_STATUS.UNPUBLISHED");
+        //enable start button
+        onStopped();
+    }).on(STREAM_STATUS.FAILED, function(stream){
+        console.log("STREAM_STATUS.FAILED", stream);
+        //enable start button
+        onStopped();*/
 
 }
