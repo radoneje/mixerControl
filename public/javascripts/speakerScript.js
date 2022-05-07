0343a6d84426c4701351e2de0d89c6f30fec6a36
radoneje <den.shevchenko@gmail.com>
2022-05-07 23:58:22 +0300

    ww

--- public/javascripts/speakerScript.js
+++ public/javascripts/speakerScript.js
@@ -13,9 +13,22 @@ var presApp=new Vue({
             localStorage.setItem('spkName', this.name||"");
             localStorage.setItem('spkPosition', this.position||"");
             this.isLogin=true;
+            activeteWebCam();
         }
     },
     mounted: async function () {
         console.log("worked")
     }
 });
+
+var serverUrl = "wss://wowza02.onevent.online:8443";
+//if(typeof(roomid)!="undefined"  && roomid>90)
+//var serverUrl = "wss://phone02.sber.link:8443";
+
+var SESSION_STATUS = Flashphoner.constants.SESSION_STATUS;
+var STREAM_STATUS = Flashphoner.constants.STREAM_STATUS;
+var STREAM_STATUS_INFO = Flashphoner.constants.STREAM_STATUS_INFO;
+
+function activeteWebCam(){
+    Flashphoner.init();
+}
