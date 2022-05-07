cd26b2d20f70b7b3c382a66882a7317db5a40320
radoneje <den.shevchenko@gmail.com>
2022-05-07 23:47:57 +0300

    ww

---
+++ public/javascripts/speakerScript.js
@@ -0,0 +1,19 @@
+var presApp=new Vue({
+    el: "#presContainer",
+    data: {
+        name: localStorage.getItem('spkName'),
+        position: localStorage.getItem('spkPosition'),
+        isLogin:false,
+    },
+    methods: {
+        login:async function(){
+            if(this.name.length==0)
+                return;
+            localStorage.setItem('spkName', this.name||"");
+            localStorage.setItem('spkPosition', this.position||"");
+            this.isLogin:true;
+        }
+    },
+    mounted: async function () {
+    }
+});
