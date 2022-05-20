974e6a1fdd87b9f16b163dc40dc771ac9170c70a
radoneje <den.shevchenko@gmail.com>
2022-05-20 15:58:41 +0300

    ww

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -260,9 +260,7 @@ router.get('/activatePresImg/:id/:eventid', async (req, res, next)=> {
 });
 router.post('/webCamPublished', async (req, res, next)=> {
 
-    var url=config.mixerCore+"mixer/startInput?id="+req.body.faceid+"&url=rtmp://wowza02.onevent.online:1935/live/streamName"
-    console.log("webCamPublished", url);
-    r=await axios.get(config.mixerCore+"mixer/startInput?id="+req.body.faceid+"&url=rtmp://wowza02.onevent.online:1935/live/"+req.body.streamName)
+    r=await axios.get(config.mixerCore+"mixer/startInput?eventid="+req.body.eventid+"&id="+req.body.faceid+"&url=rtmp://wowza02.onevent.online:1935/live/"+req.body.streamName)
     res.json(r.data);
     //http://wowza01.onevent.online:8090/mixer/startInput?id=1&url=rtmp://wowza02.onevent.online:1935/live/test
 });
