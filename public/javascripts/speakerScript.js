7ac00c5bc0d75b2164c0dba694615ef828cbbc06
radoneje <den.shevchenko@gmail.com>
2022-05-21 21:49:56 +0300

    ww

--- public/javascripts/speakerScript.js
+++ public/javascripts/speakerScript.js
@@ -14,7 +14,7 @@ var presApp = new Vue({
             console.log("login")
             if (this.name.length == 0)
                 return;
-            this.userid=await axios.Post("/api/v1/spkLogin",{eventid,name:this.name, suname:this.suname, position:this.position,userid:this.userid})
+            this.userid=await axios.post("/api/v1/spkLogin",{eventid,name:this.name, suname:this.suname, position:this.position,userid:this.userid})
                 localStorage.setItem('name', this.name || "");
                 localStorage.setItem('suname', this.suname || "");
                 localStorage.setItem('position', this.position || "");
