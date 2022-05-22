b58dc916a009bb61d9c172f69bc4973c6dc65920
radoneje <den.shevchenko@gmail.com>
2022-05-22 15:34:13 +0300

    ww

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -335,7 +335,7 @@ router.get('/inputStart/:eventid/:input/:spkid?', async (req, res, next)=> {
         ret.userid=ret.userid;
     }*/
 
-        if(req.params.spkid.length>6) {
+        if(req.params.spkid && req.params.spkid.length>6) {
             var rr = await req.knex.select("*").from("t_spklogins").where({id: req.params.spkid});
             if (rr.length > 0) {
                 ret = {name: rr[0].titlename, suname: (rr[0].titlesuname || ""), position: (rr[0].titleposition || "")};
