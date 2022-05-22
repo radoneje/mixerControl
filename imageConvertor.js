d80c4c2711a655253f0fdc8be64f7ba9dc47a26f
radoneje <den.shevchenko@gmail.com>
2022-05-22 18:21:02 +0300

    ww

--- imageConvertor.js
+++ imageConvertor.js
@@ -16,8 +16,8 @@ var app = express();
 app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.raw({
     inflate: true,
     limit: '10000kb',
