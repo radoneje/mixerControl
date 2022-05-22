03028c23e6f1f88d29bf097d3ebbaf87e6775302
radoneje <den.shevchenko@gmail.com>
2022-05-22 20:29:17 +0300

    ww

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -230,9 +230,10 @@ router.post("/addVideoLrvToPresFile/:id/", async (req, res) => {
     formData.append('fileid',fileRecord[0].id );
     formData.append('fileurl',config.uploadAlias+ path.basename(fileRecord[0].fullpath));
     formData.append('eventid', r[0].eventid);*/
-    console.log("mixer/loadPresVideo")
+    console.log("mixer/loadPresVideo",r)
     var r = await axios.get(config.mixerCore + "mixer/loadPresVideo?eventid="+r[0].eventid+"&fileid="+fileRecord[0].id+"&url="+config.uploadAlias+ path.basename(fileRecord[0].fullpath));
-   // res.json({ret: r.data, error: false});
+
+    // res.json({ret: r.data, error: false});
     console.log("mixer/loadPresVideo 1", config.mixerCore + "mixer/loadPresVideo?eventid="+r[0].eventid+"&fileid="+fileRecord[0].id+"&url="+config.uploadAlias+ path.basename(fileRecord[0].fullpath))
 
 
