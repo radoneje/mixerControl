21573c3cb9b85f9ebc49c9936aa83313c87d5557
radoneje <den.shevchenko@gmail.com>
2022-05-22 18:42:28 +0300

    ww

--- imageConvertor.js
+++ imageConvertor.js
@@ -1,4 +1,3 @@
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,7 +8,7 @@ var fs = require('fs');
 const fsPromises = fs.promises;
 var bodyParser = require('body-parser');
 var gm = require('gm');//.subClass({imageMagick: true});
-var axios=require('axios')
+var axios = require('axios')
 const pdf = require('pdf-page-counter');
 
 var app = express();
@@ -17,7 +16,7 @@ app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.raw({
     inflate: true,
     limit: '10000kb',
@@ -30,54 +29,52 @@ app.use(bodyParser.raw({
 }));
 
 
-app.use('/fullImage',async (req,res)=>{
-    var buf=req.body;//Buffer.from(req.body, 'base64')
+app.use('/fullImage', async (req, res) => {
+    var buf = req.body;//Buffer.from(req.body, 'base64')
     console.log("readFullImage", buf);
     gm(buf)
         .quality(75)
         .density(300, 300)
-        .resize(1280,720)
+        .resize(1280, 720)
         .gravity('Center')
         .background('#FFFFFF')
         .extent(1280, 720)
         .flatten()
         .setFormat('png')
         .out('+adjoin')
-        .toBuffer(async (err, buffer)=> {
+        .toBuffer(async (err, buffer) => {
             if (err) {
 
-                return console.warn("this err",err);
+                return console.warn("this err", err);
             }
-            try{
+            try {
                 await axios.post(config.callBackUrl + ":" + config.port + "/api/v1/addImageToPresFile/" + req.headers["x-folder"], buffer,
                     {headers: {'content-type': 'image/x-png'}})
-            }
-            catch (e){
+            } catch (e) {
                 console.warn(e)
             }
 
         });
     res.json("ok");
 })
-app.use('/lrvImage',async (req,res)=>{
+app.use('/lrvImage', async (req, res) => {
     console.log("readLrvImage");
     gm(req.body)
         .quality(75)
         .density(150, 150)
-        .resize(320,180)
+        .resize(320, 180)
         .gravity('Center')
         .background('#FFFFFF')
         .extent(320, 180)
         .flatten()
         .setFormat('png')
-        .toBuffer(async (err, buffer)=> {
+        .toBuffer(async (err, buffer) => {
             if (err)
                 return console.warn(err);
-            try{
-            await axios.post(config.callBackUrl + ":" + config.port + "/api/v1/addImageLrvToPresFile/" + req.headers["x-fileid"], buffer,
-                {headers: {'content-type': 'image/x-png'}})
-            }
-            catch (e){
+            try {
+                await axios.post(config.callBackUrl + ":" + config.port + "/api/v1/addImageLrvToPresFile/" + req.headers["x-fileid"], buffer,
+                    {headers: {'content-type': 'image/x-png'}})
+            } catch (e) {
                 console.warn(e)
             }
 
@@ -85,43 +82,43 @@ app.use('/lrvImage',async (req,res)=>{
     res.json("ok");
 })
 
-app.use('/pdf',async (req,res)=>{
+app.use('/pdf', async (req, res) => {
 
-    var page=1;
-    var pdfData=await pdf(req.body);
+    var page = 1;
+    var pdfData = await pdf(req.body);
 
     //await convertPDFPage(2);
     //await convertPDFPage(3);
-    var arr=[]
-    for(var i=0; i<  pdfData.numpages; i++){
-       arr.push(i);
+    var arr = []
+    for (var i = 0; i < pdfData.numpages; i++) {
+        arr.push(i);
     }
-    for(var page of arr){
+    for (var page of arr) {
         await convertPDFPage(page);
     }
-    function convertPDFPage(page){
-        return new Promise((resolve, reject)=>{
+
+    function convertPDFPage(page) {
+        return new Promise((resolve, reject) => {
             gm(req.body)
                 .selectFrame(page)
                 .quality(75)
                 .density(300, 300)
-                .resize(1280,720)
+                .resize(1280, 720)
                 .gravity('Center')
                 .background('#FFFFFF')
                 .extent(1280, 720)
                 .flatten()
                 .setFormat('png')
-                .toBuffer(async (err, buffer)=>{
-                    if(err)
-                        return  console.warn(err);
+                .toBuffer(async (err, buffer) => {
+                    if (err)
+                        return console.warn(err);
 
                     try {
-                        await axios.post(config.callBackUrl + ":" + config.port + "/api/v1/addImageToPresFolder/" + req.headers["x-presid"]+"/"+page, buffer,
+                        await axios.post(config.callBackUrl + ":" + config.port + "/api/v1/addImageToPresFolder/" + req.headers["x-presid"] + "/" + page, buffer,
                             {headers: {'content-type': 'application/pdf'}}
                         );
                         resolve();
-                    }
-                    catch (e){
+                    } catch (e) {
                         console.warn(e);
                         reject(e);
                     }
@@ -131,50 +128,62 @@ app.use('/pdf',async (req,res)=>{
     }
 
 
-        //.write('/var/www/mixerControl/public/resize1.png', function (err) {
+    //.write('/var/www/mixerControl/public/resize1.png', function (err) {
 
 
     res.json("ok");
 
 });
-app.use('/video', async (req,res)=>{
+app.use('/video', async (req, res) => {
     console.log("video file ", req.body.url, req.body);
     res.json(true)
     var child_process = require('child_process');
-    var encoder = child_process.spawn('ffmpeg',['-i', req.body.url, "-ss", "00:00:01.000", "-vframes", "1", "-y", "/tmp/"+req.body.presid+".png"]);
+    var encoder = child_process.spawn('ffmpeg', ['-i', req.body.url, "-ss", "00:00:01.000", "-vframes", "1", "-y", "/tmp/" + req.body.presid + ".png"]);
 
-   encoder.stderr.addListener('data', function(data) {
+    encoder.stderr.addListener('data', function (data) {
         console.log(data.toString())
-    }); /*
-    console.log(fs.statSync("/tmp/"+req.body.presid+".png"))
-    gm("/tmp/"+req.body.presid+".png")
+    });
+    encoder.stderr.addListener('exit', function (data) {
+        console.log(fs.statSync("/tmp/" + req.body.presid + ".png"))
+        gm("/tmp/" + req.body.presid + ".png")
+            .quality(75)
+            .density(150, 150)
+            .resize(320, 180)
+            .gravity('Center')
+            .background('#FFFFFF')
+            .extent(320, 180)
+            .flatten()
+            .setFormat('png')
+    });
+
+   /*
+    gm("/tmp/" + req.body.presid + ".png")
         .quality(75)
         .density(150, 150)
-        .resize(320,180)
+        .resize(320, 180)
         .gravity('Center')
         .background('#FFFFFF')
         .extent(320, 180)
         .flatten()
         .setFormat('png')
-        .toBuffer(async (err, buffer)=> {
+        .toBuffer(async (err, buffer) => {
             if (err)
                 return console.warn(err);
-            try{
-                fs.rm("/tmp/"+req.body.presid+".png");
+            try {
+                fs.rm("/tmp/" + req.body.presid + ".png");
                 await axios.post(config.callBackUrl + ":" + config.port + "/api/v1/addVideoLrvToPresFile/" + req.headers["x-fileid"], buffer,
                     {headers: {'content-type': 'image/x-png'}})
-            }
-            catch (e){
+            } catch (e) {
                 console.warn(e)
             }
 
         });
 */
-    console.log("/tmp/"+req.body.presid+".png");
+
 
 });
 var server = http.createServer(app);
-server.listen(config.pdfConverterPort, ()=>{
-    console.log("encoder server listen on "+ config.pdfConverterPort)
+server.listen(config.pdfConverterPort, () => {
+    console.log("encoder server listen on " + config.pdfConverterPort)
 
 })
