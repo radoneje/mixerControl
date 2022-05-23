5e9d8d7222e0a0f0eeeb62e5e360528c3ab2bca8
radoneje <den.shevchenko@gmail.com>
2022-05-23 18:20:08 +0300

    ww

--- public/javascripts/speakerScript.js
+++ public/javascripts/speakerScript.js
@@ -8,7 +8,7 @@ var presApp = new Vue({
         loginid:null,
         isLogin: false,
         eventStatus:-1,
-        needRescale:false
+        needRescale:true,
     },
     methods: {
         login: async function () {
